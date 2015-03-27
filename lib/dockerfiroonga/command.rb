68bca21f4f3deaa51605bd3ff242a7949631df10
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-27 23:32:04 +0900

    Extract a method

--- lib/dockerfiroonga/command.rb
+++ lib/dockerfiroonga/command.rb
@@ -20,25 +20,8 @@ Usage: dockerfiroonga [OPTIONS] PLATFORM [Xroonga]
     end
 
     def initialize(arguments)
-      @options = {}
-      parser = OptionParser.new(<<-END_OF_BANNER)
-#{USAGE.chomp}
-  OPTIONS:
-      END_OF_BANNER
-      parser.version = VERSION
-      parser.on("-h", "--help", "Show usage") do |boolean|
-        $stdout.puts(parser.help)
-        exit(true)
-      end
-      parser.on("--maintainer=NAME", "Set maintainer") do |name|
-        @options[:maintainer] = name
-      end
-      parser.parse!(arguments)
+      @options = parse_options(arguments)
 
-      if arguments.empty?
-        $stdout.puts(parser.help)
-        exit(true)
-      end
       @platform_name = arguments[0]
       @platform = Platform.new(@platform_name)
       @_roonga = arguments[1] || "groonga"
@@ -56,6 +39,31 @@ MAINTAINER #{@maintainer}
 #{@platform.__send__("installation_#{@_roonga}")}
 CMD ["groonga", "--version"]
       END_OF_FILE
+    end
+
+    private
+    def parse_options(arguments)
+      options = {}
+      parser = OptionParser.new(<<-END_OF_BANNER)
+#{USAGE.chomp}
+  OPTIONS:
+      END_OF_BANNER
+      parser.version = VERSION
+      parser.on("-h", "--help", "Show usage") do |boolean|
+        $stdout.puts(parser.help)
+        exit(true)
+      end
+      parser.on("--maintainer=NAME", "Set maintainer") do |name|
+        options[:maintainer] = name
+      end
+      parser.parse!(arguments)
+
+      if arguments.empty?
+        $stdout.puts(parser.help)
+        exit(true)
+      end
+
+      options
     end
   end
 end
